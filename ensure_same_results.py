f28b4297f229f4d16d264f3c165746664ab6bd25
Marco Ciotola <848222@stud.unive.it>
2025-01-28 22:02:08 +0100

    fix: results comparison and terminology

--- ensure_same_results.py
+++ ensure_same_results.py
@@ -32,47 +32,47 @@ def compare_results(data1, data2):
     columns1, data1 = data1
     columns2, data2 = data2
     if any(map(lambda x: x not in columns1, columns2)):
-        print('Missing stat in ours')
+        print('Missing stat in curr')
         print(set(columns2) - set(columns1))
-        return
     if any(map(lambda x: x not in data1.keys(), data2.keys())):
-        print('Missing keys in ours')
+        print('Missing keys in curr')
         print(set(data2.keys()) - set(data1.keys()))
-        return
     different = False
     for key in data2.keys():
+        if key not in data1:
+            continue
         key_header = False
         duration_diff = ""
         for column in columns2[1:]:
-            if column.endswith('ConfInt') or not data2[key][column]:
+            if not column or column not in data1[key] or column.endswith('ConfInt') or not data2[key][column]:
                 continue
-            ours = float(data1[key][column])
-            theirs = float(data2[key][column])
-            if not math.isclose(ours, theirs, rel_tol=1e-3):
+            curr = float(data1[key][column])
+            prev = float(data2[key][column])
+            if not math.isclose(curr, prev, rel_tol=1e-3):
                 if column == 'Run Duration':
-                    if ours < theirs:
-                        if ours == 0:
-                            duration_diff = f'Speedup: {theirs}/0'
+                    if curr < prev:
+                        if curr == 0:
+                            duration_diff = f'Speedup: {prev}/0'
                         else:
-                            duration_diff = f'Speedup: {(1 - ours / theirs) * 100:.2f}%'
+                            duration_diff = f'Speedup: {(1 - curr / prev) * 100:.2f}%'
                     else:
-                        duration_diff = f'Slowdown: {(ours / theirs - 1) * 100:.2f}%'
+                        duration_diff = f'Slowdown: {(curr / prev - 1) * 100:.2f}%'
                     continue
                 if not key_header:
                     print(f'{columns2[0]}: {key}')
                     key_header = True
-                print(f'\tColumn: {column}')
+                print(f'\t{column}:')
                 if "Stability Check" not in column:
-                    print(f'\t\t  ours: {data1[key][column]} {data1[key][column + " ConfInt"]}')
-                    print(f'\t\ttheirs: {data2[key][column]} {data2[key][column + " ConfInt"]}')
+                    print(f'\t\tcurr: {data1[key][column]} {data1[key][column + " ConfInt"]}')
+                    print(f'\t\tprev: {data2[key][column]} {data2[key][column + " ConfInt"]}')
                 else:
-                    print(f'\t\t  ours: {data1[key][column]}')
-                    print(f'\t\ttheirs: {data2[key][column]}')
+                    print(f'\t\tcurr: {data1[key][column]}')
+                    print(f'\t\tprev: {data2[key][column]}')
                 different = True
         if print_speedup and duration_diff:
             if not key_header:
                 print(f'{columns2[0]}: {key}')
-            print(f'\t{duration_diff} (ours: {data1[key]["Run Duration"]} ; theirs: {data2[key]["Run Duration"]})')
+            print(f'\t{duration_diff} (curr: {data1[key]["Run Duration"]} ; prev: {data2[key]["Run Duration"]})')
     if not different:
         print('Data is the same')
 
