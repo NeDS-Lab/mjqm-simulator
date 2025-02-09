70c78cca84e4c99a8bdbd087c35d1ff99472095d
Marco Ciotola <848222@stud.unive.it>
2025-02-09 21:48:04 +0100

    fix: cleaner and hardened code

--- ensure_same_results.py
+++ ensure_same_results.py
@@ -5,16 +5,19 @@ from pathlib import Path
 import math
 
 os.environ.setdefault("PRINT_SPEEDUP", "0")
-
 print_speedup = os.environ.get("PRINT_SPEEDUP") == "1"
 
 
 def load_results(path: Path):
     data = {}
-    with open(path, 'r') as file:
-        columns = file.readline().strip().split(';')
+    with open(path, "r") as file:
+        loaded_columns = file.readline().strip().split(";")
+        columns = []
+        for column in loaded_columns:
+            column = column.replace("Lenght", "Length")
+            columns.append(column)
         for line in file:
-            values = line.strip().split(';')
+            values = line.strip().split(";")
             if values[0]:
                 data[values[0]] = {}
                 for column, value in zip(columns[1:], values[1:]):
@@ -25,20 +28,24 @@ def load_results(path: Path):
 def print_with_columns_key(columns, data, filter_column=None):
     for key, values in data.items():
         print()
-        print(f'{columns[0]}: {key}')
+        print(f"{columns[0]}: {key}")
         for column, value in zip(columns[1:], values):
             if filter_column is None or filter_column(column):
-                print(f'{column}: {value}')
+                print(f"{column}: {value}")
+
+
+def divergence(curr: float, prev: float):
+    return ((curr / prev) - 1) * 100 if prev != 0 else 100
 
 
 def compare_results(data1, data2):
     columns1, data1 = data1
     columns2, data2 = data2
     if any(map(lambda x: x not in columns1, columns2)):
-        print('Missing stat in curr')
+        print("Missing stat in curr")
         print(set(columns2) - set(columns1))
     if any(map(lambda x: x not in data1.keys(), data2.keys())):
-        print('Missing keys in curr')
+        print("Missing keys in curr")
         print(set(data2.keys()) - set(data1.keys()))
     different = False
     for key in data2.keys():
@@ -47,7 +54,13 @@ def compare_results(data1, data2):
         key_header = False
         duration_diff = ""
         for column in columns2[1:]:
-            if not column or column not in data1[key] or column.endswith('ConfInt') or not data2[key][column] or not data1[key][column]:
+            if (
+                not column
+                or column not in data1[key]
+                or column.endswith("ConfInt")
+                or not data2[key][column]
+                or not data1[key][column]
+            ):
                 continue
             curr_s = data1[key][column]
             prev_s = data2[key][column]
@@ -56,35 +69,36 @@ def compare_results(data1, data2):
             close = curr_s == prev_s
             close = close or math.isclose(curr, prev, rel_tol=1e-3)
             if not close:
-                if column == 'Run Duration':
-                    if prev == 0:
-                        duration_diff = f'Slowdown: 100.00%'
-                    elif curr < prev:
-                        duration_diff = f' Speedup: {(1 - curr / prev) * 100:.2f}%'
+                different = True
+                change = divergence(curr, prev)
+                if column == "Run Duration":
+                    if change < 0:
+                        duration_diff = f" Speedup: {-change:+.2f}%"
                     else:
-                        duration_diff = f'Slowdown: {(curr / prev - 1) * 100:.2f}%'
+                        duration_diff = f"Slowdown: {-change:+.2f}%"
                     continue
                 if not key_header:
-                    print(f'{columns2[0]}: {key}')
+                    print(f"{columns2[0]}: {key}")
                     key_header = True
-                print(f'\t{column}:')
-                print(f'\t\tdivergence: {(((curr / prev) - 1) * 100 if prev != 0 else 100):+.2f}%')
+                print(f"\t{column}:")
+                print(f"\t\tdivergence: {change:+.2f}%")
                 if "Stability Check" not in column:
-                    print(f'\t\tcurr: {data1[key][column]} {data1[key][column + " ConfInt"]}')
-                    print(f'\t\tprev: {data2[key][column]} {data2[key][column + " ConfInt"]}')
+                    print(f"\t\tcurr: {data1[key][column]} {data1[key][column + ' ConfInt']}")
+                    print(f"\t\tprev: {data2[key][column]} {data2[key][column + ' ConfInt']}")
                 else:
-                    print(f'\t\tcurr: {data1[key][column]}')
-                    print(f'\t\tprev: {data2[key][column]}')
-                different = True
+                    print(f"\t\tcurr: {data1[key][column]}")
+                    print(f"\t\tprev: {data2[key][column]}")
         if print_speedup and duration_diff:
             if not key_header:
-                print(f'{columns2[0]}: {key}')
-            print(f'\t{duration_diff} (curr: {data1[key]["Run Duration"]} ; prev: {data2[key]["Run Duration"]})')
+                print(f"{columns2[0]}: {key}")
+            print(
+                f"\t{duration_diff} (curr: {data1[key]['Run Duration']} ; prev: {data2[key]['Run Duration']})"
+            )
     if not different:
-        print('Data is the same')
+        print("Data is the same")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # get the two paths from arguments
     path1 = Path(sys.argv[1])
     path2 = Path(sys.argv[2])
--- simulator_toml.cpp
+++ simulator_toml.cpp
@@ -27,31 +27,38 @@ void run_simulation(const ExperimentConfig& conf,
 }
 
 int main(int argc, char* argv[]) {
-    std::vector<unsigned int> sizes;
-    std::vector<std::string> headers{"Arrival Rate"};
     std::string input_name(argv[1]);
     auto overrides = parse_overrides_from_args(argc, argv);
     auto experiments = from_toml("Inputs/" + input_name + ".toml", overrides);
-    if (experiments->empty() || !experiments->at(0).first) {
-        std::cerr << "Error reading TOML file" << std::endl;
+    if (experiments->empty()) {
+        std::cerr << "The provided identifier doesn't generate any configuration" << std::endl;
         return 1;
     }
 
-    auto n_experiments = experiments->size();
+    size_t n_experiments = experiments->size();
+
+    for (int i = 0; i < n_experiments; ++i) {
+        if (!experiments->at(i).first) {
+            std::cerr << "Error reading TOML file" << std::endl;
+            return 1;
+        }
+    }
 
     std::vector<ExperimentStats> experiments_stats(n_experiments);
     std::vector<std::thread> threads(n_experiments);
 
     std::unordered_map<std::string, std::ofstream> out_files;
-    for (const auto& conf : std::views::values(*experiments)) {
+    for (int i = 0; i < n_experiments; ++i) {
+        const auto& conf = experiments->at(i).second;
         std::cout << conf << std::endl;
-        std::cout << conf.toml << std::endl << std::endl;
         std::string out_filename = conf.output_filename();
         if (!out_files.contains(out_filename)) {
             out_files[out_filename] = std::ofstream(out_filename, std::ios::app);
             if (out_files[out_filename].tellp() == 0) {
+                std::vector<unsigned int> sizes;
+                std::vector<std::string> headers{"Arrival Rate"};
                 conf.get_sizes(sizes);
-                experiments_stats.at(0).add_headers(headers, sizes);
+                experiments_stats[i].add_headers(headers, sizes);
                 // Write the headers to the CSV file
                 for (const std::string& header : headers) {
                     out_files[out_filename] << header << ";";
@@ -60,18 +67,17 @@ int main(int argc, char* argv[]) {
             }
         }
     }
-
-    for (int i = 0; i < n_experiments; i++) {
+    for (int i = 0; i < n_experiments; ++i) {
         threads[i] = std::thread(run_simulation, std::ref(experiments->at(i).second), std::ref(experiments_stats[i]));
     }
-    for (int i = 0; i < n_experiments; i++) {
+    for (int i = 0; i < n_experiments; ++i) {
         threads[i].join();
     }
 
-    for (int i = 0; i < n_experiments; i++) {
-        std::string out_filename = experiments->at(i).second.output_filename();
-        out_files[out_filename] << experiments->at(i).second.toml.at_path("arrival.rate").value<double>().value()
-                                << ";";
+    for (int i = 0; i < n_experiments; ++i) {
+        const auto& conf = experiments->at(i).second;
+        std::string out_filename = conf.output_filename();
+        out_files[out_filename] << conf.toml.at_path("arrival.rate").value<double>().value() << ";";
         out_files[out_filename] << experiments_stats[i] << "\n";
     }
 
