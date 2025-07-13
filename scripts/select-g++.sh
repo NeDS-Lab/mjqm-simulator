c94ee55154f6b934eb22120bf7f3e65f9c117b81
Marco Ciotola <github@ciotola.dev>
2025-07-13 13:44:56 +0200

    ci: probably fix compiler choice

--- scripts/select-g++.sh
+++ scripts/select-g++.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 # Select the g++ version to use
+if [ ! -z "$GCC_COMPILER" ]; then
+    if command -version $GCC_COMPILER >/dev/null ; then
+        echo -n $GCC_COMPILER ;
+        exit 0 ;
+    fi
+fi
 
 for binary in g++-14 g++-13 g++-12 g++-11 g++-10 g++ ; do
     if command -v $binary >/dev/null ; then
