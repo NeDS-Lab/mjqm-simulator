70f23b821e1ba02bca3c14e7a571c9a708c2d399
Marco Ciotola <github@ciotola.dev>
2025-07-13 13:52:32 +0200

    fix: select-g++

--- scripts/select-g++.sh
+++ scripts/select-g++.sh
@@ -2,15 +2,15 @@
 
 # Select the g++ version to use
 if [ ! -z "$GCC_COMPILER" ]; then
-    if command -version $GCC_COMPILER >/dev/null ; then
-        echo -n $GCC_COMPILER ;
+    if command -v "$GCC_COMPILER" >/dev/null ; then
+        echo -n "$GCC_COMPILER" ;
         exit 0 ;
     fi
 fi
 
 for binary in g++-14 g++-13 g++-12 g++-11 g++-10 g++ ; do
-    if command -v $binary >/dev/null ; then
-        echo -n $binary ;
+    if command -v "$binary" >/dev/null ; then
+        echo -n "$binary" ;
         exit 0 ;
     fi
 done
