a38b8576059a73fd2d1ecfcae2c743f9f6540b68
Marco Ciotola <github@ciotola.dev>
2025-07-13 16:30:06 +0200

    fix: support gcc 15

--- scripts/select-g++.sh
+++ scripts/select-g++.sh
@@ -8,7 +8,7 @@ if [ ! -z "$GCC_COMPILER" ]; then
     fi
 fi
 
-for binary in g++-14 g++-13 g++-12 g++-11 g++-10 g++ ; do
+for binary in g++-15 g++-14 g++-13 g++-12 g++-11 g++-10 g++ ; do
     if command -v "$binary" >/dev/null ; then
         echo -n "$binary" ;
         exit 0 ;
